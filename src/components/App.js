78f074d1753680d0360b8a89772b4595e6debc67
GODELTECH\a.yaskevich <a.yaskevich@godeltech.com>
2023-05-15 16:48:44 +0200

    add header component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import HomePage from './home';
 import AboutPage from './about';
+import Header from './common/header';
 
 const App = () => {
     return (
         <div className="container-fluid">
+            <Header />
             <Route exact path="/" component={HomePage} />
             <Route exact path="/about" component={AboutPage} />
         </div>
