ba712aed8dc35d42a82e957562b3983bbd57a905
GODELTECH\a.yaskevich <a.yaskevich@godeltech.com>
2023-05-17 12:41:35 +0200

    init redux

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CoursesPage = () => {
+    const [state, setState] = useState({
+        course: {
+            title: ""
+        }
+    });
+
+    const handleChange = event => {
+        const course = {...state.course, title: event.target.value};
+        setState({course});
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(state.course.title);
+    }
+
     return (
-        <h2>Courses</h2>
+        <form onSubmit={handleSubmit}>
+            <h2>Courses</h2>
+            <h3>Add Course</h3>
+            <input type="text" onChange={handleChange} value={state.course.title} />
+            <input type="submit" value="Save" />
+        </form>
     );
 };
 
